0d1e534370fa48bd8775b3a967c1a318e086f17e
Shalom Hunukumbura <114820357+ShalomHunukumbura@users.noreply.github.com>
2024-01-12 01:27:34 +0530

    Add CI/CD workflow and update Terraform configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,11 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA2PQ7KJFJM4365Y4M"
-  secret_key = "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"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
 resource "aws_instance" "my-first-server" {
   ami           = "0014ce3e52359afbd"  # Replace with the correct AMI ID
   instance_type = "t3.micro"
