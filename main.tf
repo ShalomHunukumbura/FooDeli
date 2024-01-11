7749d1fb4deffdf1c8b995df4c8475813ae4fcdf
bat24am <155823630+bat24am@users.noreply.github.com>
2024-01-11 22:57:25 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,28 @@
 provider "aws" {
-    region = "us-east-1"
-    access_key = "AKIA2PQ7KJFJM4365Y4M"
-    secret_key = "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"
+  region     = "us-east-1"
+  access_key = "AKIA2PQ7KJFJM4365Y4M"
+  secret_key = "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"
 }
 
-resource "aws_instance" "my-first-sever" {
-  ami           ="${0014ce3e52359afbd}"
-  instance_type ="t3.micro"
+resource "aws_instance" "my-first-server" {
+  ami           = "0014ce3e52359afbd"  # Replace with the correct AMI ID
+  instance_type = "t3.micro"
 
-  tags ={
-    Name =""
+  tags = {
+    Name = "my-first-server"
+  }
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-cloud-cruiser"
+  acl    = "private"  
+
+versioning {
+    enabled = true
   }
 
-  # ...
+  tags = {
+    Name        = "cloud-cruiser"
+    Environment = "Dev"
+  }
 }
